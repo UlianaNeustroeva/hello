62296b05f53cc65d3a73a84b715066c88e5fe6b9
Ulya <uliana.neustroeva@nimbledevices.com>
2023-02-14 11:55:33 +0200

    Update counter.js

--- counter.js
+++ counter.js
@@ -3,7 +3,7 @@ let counter = 0;
 
 function count() {
     counter++;
-    document.querySelector('h2').innerHTML = counter;
+    document.querySelector('h1').innerHTML = counter;
 
     if (counter % 10 === 0) {
         alert(`Count is now ${counter}`);
